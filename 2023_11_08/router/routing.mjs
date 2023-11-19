8c738568046cdf91cd1a30a4c7dec47a585f57c4
KamilPrzybylski <90440270+KamilPrzybylski@users.noreply.github.com>
2023-11-19 14:46:15 +0100

    Update routing.mjs

--- 2023_11_08/router/routing.mjs
+++ 2023_11_08/router/routing.mjs
@@ -1,25 +1,8 @@
-// import { createConnection } from 'mysql'
 import express from 'express'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
 const router = express.Router()
-
-// const conn = createConnection({
-//     host: '172.25.224.1',
-//     user: 'root',
-//     password: '',
-//     database: 'database'
-// })
-
-// conn.connect((e) => {
-//     if(e) {
-//         throw e
-//     }
-//     else {
-//         console.log('Connected to database')
-//     }
-// })
 
 const ERR_404 = {
     'Error 404': 'Page not found'
@@ -50,21 +33,6 @@ router.get('/students', async (req, res) => {
     else {
         res.json(students)
     }
-
-    // const students = []
-    // const sql = "SELECT * FROM students;"
-    // conn.query(sql, (e, result)=>{
-    //     if(e) {
-    //         throw e
-    //     }
-    //     else {
-    //         result.forEach((res) => {
-    //             const result = JSON.parse(JSON.stringify(res))
-    //             students.push(result)
-    //         })
-    //         res.json(students)
-    //     }   
-    // })
 })
 
 router.get('/subjects', async (req, res) => {
@@ -76,21 +44,6 @@ router.get('/subjects', async (req, res) => {
     else {
         res.json(subjects)
     }
-
-    // const subjects = []
-    // const sql = "SELECT * FROM subjects;"
-    // conn.query(sql, (e, result)=>{
-    //     if(e) {
-    //         throw e
-    //     }
-    //     else {
-    //         result.forEach((res) => {
-    //             const result = JSON.parse(JSON.stringify(res))
-    //             subjects.push(result)
-    //         })
-    //         res.json(subjects)
-    //     }   
-    // })
 })
 
 router.get('/students/:id', async (req, res) => {
@@ -107,27 +60,6 @@ router.get('/students/:id', async (req, res) => {
     else {
         res.json(student)
     }
-
-    // const student = []
-    // const sql = `SELECT * FROM students WHERE id = ${id};`
-    // conn.query(sql, (e, result)=>{
-    //     if(e) {
-    //         throw e
-    //     }
-    //     else {
-    //         if (result.length) {
-    //             result.forEach((res) => {
-    //                 const result = JSON.parse(JSON.stringify(res))
-    //                 student.push(result)
-    //             })
-    //             res.json(student)
-    //         }
-    //         else {
-    //             res.status(404)
-    //             res.json(ERR_404)
-    //         }
-    //     }   
-    // })
 })
 
 router.get('/subjects/:id', async (req, res) => {
@@ -144,27 +76,6 @@ router.get('/subjects/:id', async (req, res) => {
     else {
         res.json(subject)
     }
-
-    // const subject = []
-    // const sql = `SELECT * FROM subjects WHERE id = ${id};`
-    // conn.query(sql, (e, result)=>{
-    //     if(e) {
-    //         throw e
-    //     }
-    //     else {
-    //         if (result.length) {
-    //             result.forEach((res) => {
-    //                 const result = JSON.parse(JSON.stringify(res))
-    //                 subject.push(result)
-    //             })
-    //             res.json(subject)
-    //         }
-    //         else {
-    //             res.status(404)
-    //             res.json(ERR_404)
-    //         }
-    //     }   
-    // })
 })
 
 export {router as apiRouter}
