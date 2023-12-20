7703a75b7a80d7106432ac2aa21b9db0343f9b38
KamilPrzybylski <90440270+KamilPrzybylski@users.noreply.github.com>
2023-12-21 04:41:10 +1400

    Add files via upload

---
+++ 2023_12_20/src/helpers/routes.tsx
@@ -0,0 +1,24 @@
+import React from "react"
+import Homepage from "../pages/Homepage"
+import About from "../pages/About"
+
+interface RouteElement {
+    path: string
+    element: React.JSX.Element
+    title: string
+}
+
+//export const routes: RouteElement[] = [] (Pablo nie poleca)
+
+export const routes: Array<RouteElement> = [
+    {
+        path: "/",
+        element: <Homepage/>,
+        title: "Homepage"
+    },
+    {
+        path: "/about",
+        element: <About/>,
+        title: "About Us"
+    }
+]
